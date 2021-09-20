ca7355b86c00aa1b45176a80f758837842642d6f
Kingsley Victor <javaprodigy56@gmail.com>
2021-09-20 13:49:32 +0100

    new commit

--- contracts/CrowdSale.sol
+++ contracts/CrowdSale.sol
@@ -5,18 +5,19 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract CrowdSale is Context, Ownable {
-  /// The Doge2 token
   IERC20 _token;
-
-  /// Rate
   uint256 _rate;
+  address payable _fundAddress;
+  uint256 _startTime;
+  uint256 _endTime;
+  bool _initialized;
+  bool _finalized;
 
   struct PaymentRecord {
     uint256 _amount;
     uint256 _withdrawalTime;
   }
 
-  /// A map of addresses to amount of tokens that can be withdrawn in a month
   mapping(address => PaymentRecord) _withdrawalList;
   mapping(address => uint256) _buyRecord;
 
@@ -28,8 +29,35 @@ contract CrowdSale is Context, Ownable {
     _;
   }
 
-  constructor(address token_, uint256 rate_) public Ownable() {
+  modifier onlyFundAddress() {
+    require(
+      _msgSender() == _fundAddress,
+      "Crowdsale: Only fund address can execute this function"
+    );
+    _;
+  }
+
+  event PaymentMade(address buyer, uint256 amount);
+  event RateChanged(uint256 newRate);
+
+  constructor(
+    address token_,
+    uint256 rate_,
+    address fundAddress_
+  ) public Ownable() {
     _token = IERC20(token_);
     _rate = rate_;
+    _fundAddress = payable(fundAddress_);
+  }
+
+  function _incrementRecordAmount(uint256 _amount) private {
+    _buyRecord[_msgSender()] = _buyRecord[_msgSender()] + _amount;
+  }
+
+  function setRate(uint256 rate_) external onlyFundAddress returns (bool) {
+    require(rate_ > 0, "Crowdsale: Rate must be greater than 0");
+    _rate = rate_;
+    emit RateChanged(rate_);
+    return true;
   }
 }
